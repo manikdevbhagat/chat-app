43bc8ab20dc7267e760c4507ccdffc8b33f1f4d8
Manik <manikdevbhagat@gmail.com>
2023-03-15 09:36:07 +0000

    Created database model for messages

---
+++ server/models/messageModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  room: {
+    type: String,
+    required: true,
+  },
+  __createdtime__: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;    
